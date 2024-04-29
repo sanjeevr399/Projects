9f08972854ae4eeed23bec691fbd8837c5982ea2
Sanjeev Ranjan <sanjeevr@infinite.com>
2024-04-29 12:14:42 +0530

    Assesment_2

--- Assessments/Assesment_2/MVC_Solution_1/MVC_Solution_1/Controllers/SolutionController.cs
+++ Assessments/Assesment_2/MVC_Solution_1/MVC_Solution_1/Controllers/SolutionController.cs
@@ -1,34 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using MVC_Solution_1.Models;
-
-//namespace MVC_Solution_1.Controllers
-//{
-//    public class SolutionController : Controller
-//    {
-//        //private NorthwindEntities db = new NorthwindEntities();
-//        private northwindEntities db = new northwindEntities();
-//        // GET: Solution
-//        public ActionResult Index()
-//        {
-//            var germanCustomers = db.Customers.Where(c => c.Country == "Germany").ToList();
-//            return View(germanCustomers);
-//            //return View();
-//        }
-//        public ActionResult CustomerWithOrder10248()
-//        {
-//            var customer = db.Customers.FirstOrDefault(c => c.Orders.Any(o => o.OrderID == 10248));
-//            return View(customer);
-//        }
-//    }
-//}
-
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
